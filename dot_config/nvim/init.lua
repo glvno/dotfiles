0243bf3152b6dc9912b707cd80124929e5df8603
Michael Glaviano <michael.glaviano@consumercellular.com>
2022-12-26 11:51:18 -0800

    Update .config/nvim/init.lua Update .config/nvim/lua/custom/plugins.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,17 +1,28 @@
 vim.g.is_unix = vim.loop.os_uname().sysname == "Darwin"
+vim.g.mapleader = " "
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+local plugins = require('custom.plugins')
+require("lazy").setup(plugins)
 
 if not vim.g.is_unix then
   vim.env.PATH = 'C:\\Users\\michael.glaviano\\AppData\\Roaming\\nvm\\v19.3.0;' .. vim.env.PATH
 end
 
 local start_time
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd [[packadd packer.nvim]]
-end
 
 
 local before_init = function()
@@ -19,96 +30,14 @@ local before_init = function()
   vim.cmd[[colorscheme gruvbox-material]]
 end
 
-require('packer').startup(function(use)
-  -- Package manager
-  use 'wbthomason/packer.nvim'
-
-  use { -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    requires = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      'j-hui/fidget.nvim',
-    },
-  }
-
-  use { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  }
-
-  use { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    requires = { 'p00f/nvim-ts-rainbow' },
-    run = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
-  }
-
-  use { -- Additional text objects via treesitter
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
-  }
-
-  -- Git related plugins
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb'
-  use 'lewis6991/gitsigns.nvim'
-
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
-
-  -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-  use {'nvim-orgmode/orgmode' } 
-  local orgmode = require('orgmode')
-  local default_notes = [[C:\Users\michael.glaviano\.local\share\notes\refile.org]]
-  local agenda_files = { [[C:\Users\michael.glaviano\.local\share\notes\*]] }
-  orgmode.setup({
-    org_agenda_files = agenda_files,
-    org_default_notes_file = default_notes
-  })
-
-  -- Add custom plugins to packer from /nvim/lua/custom/plugins.lua
-  local has_plugins, plugins = pcall(require, 'custom.plugins')
-  if has_plugins then
-    plugins(use)
-  end
-
-  if is_bootstrap then
-    require('packer').sync()
-  end
-end)
+-- Package manager
 
 -- When we are bootstrapping a configuration, it doesn't
 -- make sense to execute the rest of the init.lua.
 --
 -- You'll need to restart nvim, and then it will work.
-if is_bootstrap then
-  print '=================================='
-  print '    Plugins are being installed'
-  print '    Wait until Packer completes,'
-  print '       then restart nvim'
-  print '=================================='
-  return
-end
 
 -- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -171,51 +100,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- Set lualine as statusline
 -- See `:help lualine.txt`
-require('lualine').setup {
-  options = {
-    theme = 'gruvbox',
-  },
-}
 
 -- Enable Comment.nvim
-require('Comment').setup()
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
-}
 
 -- Gitsigns
 -- See `:help gitsigns.txt`
-require('gitsigns').setup {
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
-}
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-  extensions = {
-    file_browser = {
-      hijack_netrw = true
-    }
-  },
-  defaults = {
-    mappings = {
-      i = {
-	['<C-u>'] = false,
-	['<C-d>'] = false,
-      },
-    },
-  },
-}
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
@@ -518,15 +413,15 @@ local liblldb_path = extension_path .. '/lldb/lib/liblldb.dylib'
 
 local rt = require("rust-tools")
 local opts = {
-	-- ... other configs
-	server = {
-	  on_attach = on_attach,
-	  before_init = before_init
-	},
-	dap = {
-		adapter = require('rust-tools.dap').get_codelldb_adapter(
-			codelldb_path, liblldb_path)
-	}
+  -- ... other configs
+  server = {
+    on_attach = on_attach,
+    before_init = before_init
+  },
+  dap = {
+    adapter = require('rust-tools.dap').get_codelldb_adapter(
+      codelldb_path, liblldb_path)
+  }
 }
 
 -- Normal setup
--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -1,79 +1,196 @@
-return function(use)
-	use {
+return {
+
+	{ -- LSP Configuration & Plugins
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			'williamboman/mason.nvim',
+			'williamboman/mason-lspconfig.nvim',
+
+			-- Useful status updates for LSP
+			'j-hui/fidget.nvim',
+		},
+	},
+
+	{ -- Autocompletion
+		'hrsh7th/nvim-cmp',
+		dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+	},
+
+	{ -- Highlight, edit, and navigate code
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = { 'p00f/nvim-ts-rainbow' },
+		build = function()
+			pcall(require('nvim-treesitter.install').update { with_sync = true })
+		end,
+	},
+
+	{ -- Additional text objects via treesitter
+		'nvim-treesitter/nvim-treesitter-textobjects',
+		dependencies = 'nvim-treesitter',
+	},
+
+	-- Git related plugins
+	'tpope/vim-fugitive',
+	'tpope/vim-rhubarb',
+	{
+		'lewis6991/gitsigns.nvim',
+		config = function()
+			require('gitsigns').setup {
+				signs = {
+					add = { text = '+' },
+					change = { text = '~' },
+					delete = { text = '_' },
+					topdelete = { text = '‾' },
+					changedelete = { text = '~' },
+				},
+			}
+		end
+
+	},
+
+	{
+		'nvim-lualine/lualine.nvim', -- Fancier statusline
+		config = function()
+			require('lualine').setup {
+				options = {
+					theme = 'gruvbox',
+				},
+			}
+		end
+	},
+	{
+		'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+		config = function()
+			require('indent_blankline').setup {
+				char = '┊',
+				show_trailing_blankline_indent = false,
+			}
+		end
+	},
+	{
+		'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
+		config = function()
+			require('Comment').setup()
+		end
+
+	},
+	'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+	-- Fuzzy Finder (files, lsp, etc)
+	{ 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			require('telescope').setup {
+				extensions = {
+					file_browser = {
+						hijack_netrw = true
+					}
+				},
+				defaults = {
+					mappings = {
+						i = {
+							['<C-u>'] = false,
+							['<C-d>'] = false,
+						},
+					},
+				},
+			}
+		end
+	},
+
+	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+	{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
+	{'nvim-orgmode/orgmode',
+		config = function()
+			local orgmode = require('orgmode')
+			local default_notes = [[C:\Users\michael.glaviano\.local\share\notes\refile.org]]
+			local agenda_files = { [[C:\Users\michael.glaviano\.local\share\notes\*]] }
+			orgmode.setup({
+				org_agenda_files = agenda_files,
+				org_default_notes_file = default_notes
+			})
+	end },
+
+
+	{
 		'phaazon/hop.nvim',
 		branch = 'v2', -- optional but strongly recommended
 		config = function()
 			-- you can configure hop the way you like here; see :h hop-config
 			require('hop').setup { keys = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz' }
 		end
-	}
-	use {
-		"windwp/nvim-autopairs",
+	},
+
+	{	"windwp/nvim-autopairs",
 		config = function() require("nvim-autopairs").setup {} end
-	}
-	use {
-		"windwp/nvim-ts-autotag",
+	},
+
+	{	"windwp/nvim-ts-autotag",
 		config = function() require("nvim-ts-autotag").setup {} end
-	}
-	use {'akinsho/bufferline.nvim',
-		tag = "v3.*",
-		requires = 'nvim-tree/nvim-web-devicons',
+	},
+	{
+		'akinsho/bufferline.nvim',
+		version = "v3.*",
 		config = function()
-			require('bufferline').setup{ options = { show_tab_indicators = true }} end }
-	use { 'shaunsingh/nord.nvim' }
-	use {'sainnhe/gruvbox-material'}
-	use { "ellisonleao/gruvbox.nvim" }
+			require('bufferline').setup{ options = { show_tab_indicators = true }} end },
+	{	 'shaunsingh/nord.nvim' },
+	{'sainnhe/gruvbox-material'},
+	{"ellisonleao/gruvbox.nvim",
+		config = function()
 
-	require("gruvbox").setup({
-		undercurl = true,
-		underline = true,
-		bold = true,
-		italic = true,
-		strikethrough = true,
-		invert_selection = false,
-		invert_signs = false,
-		invert_tabline = false,
-		invert_intend_guides = false,
-		inverse = true, -- invert background for search, diffs, statuslines and errors
-		contrast = "soft", -- can be "hard", "soft" or empty string
-		palette_overrides = {},
-		overrides = {},
-		dim_inactive = false,
-		transparent_mode = false,
-	})
-	vim.cmd("colorscheme gruvbox")
+			require("gruvbox").setup({
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = true,
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_intend_guides = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = "soft", -- can be "hard", "soft" or empty string
+				palette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = false,
+			})
+			vim.cmd("colorscheme gruvbox")
+	end },
 
-	-- use { 'tpope/vim-surround' }
-	use({
+	{
 		"kylechui/nvim-surround",
-		tag = "*", -- use for stability; omit to use `main` branch for the latest features
+		version = "*", -- use for stability; omit to use `main` branch for the latest features
 		config = function()
 			require("nvim-surround").setup({
 				-- configuration here, or leave empty to use defaults
 			})
 		end
-	})
-	use { 'hoffs/omnisharp-extended-lsp.nvim'}
-	use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
+	},
+	{ 'hoffs/omnisharp-extended-lsp.nvim'},
+	{'sindrets/diffview.nvim', dependencies = 'nvim-lua/plenary.nvim' },
 
 
-	use { 'mbbill/undotree' }
-	use {
-		'j-hui/fidget.nvim',
-		config = require('fidget').setup{}
-	}
+	{ 'mbbill/undotree' },
 
-	use {
-		'nvim-treesitter/nvim-treesitter-context',
-	}
-	use { 'nvim-telescope/telescope-file-browser.nvim'}
-	use 'f-person/git-blame.nvim'
-	use 'mfussenegger/nvim-dap'
-	use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-	use 'simrat39/rust-tools.nvim'
-	use {
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
+	{'j-hui/fidget.nvim',
+		config = function()
+			require('fidget').setup()
+		end
+	},
+
+
+	{'nvim-treesitter/nvim-treesitter-context'},
+	{'nvim-telescope/telescope-file-browser.nvim'},
+	'f-person/git-blame.nvim',
+	'mfussenegger/nvim-dap',
+	{ "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap"} },
+	{
+		'simrat39/rust-tools.nvim',
+	},
+	'kyazdani42/nvim-web-devicons',
+
+	{"folke/trouble.nvim",
 		config = function()
 			require("trouble").setup {
 				-- your configuration comes here
@@ -81,11 +198,11 @@ return function(use)
 				-- refer to the configuration section below
 			}
 		end
-	}
-	use({
+	},
+	{
 		"maan2003/lsp_lines.nvim",
 		config = function()
 			require("lsp_lines").setup()
 		end,
-	})
-end
+	},
+}
